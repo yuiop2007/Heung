fc92435f3a947e1ccbe7e59316514e82d03ac34f
green <green@SC-202103101110>
2021-04-29 17:00:55 +0900

    20210429html,css3,js

---
+++ css/0428/test.js
@@ -0,0 +1,15 @@
+'use strict'
+document.write("이곳은 외부 자바스크립트");
+
+// var aa = 10;
+// document.write(aa);
+
+let aa= 10;
+document.write(aa);
+
+const MAX = 100; //상수선언
+document.write(MAX);
+
+function temp2(){
+    alert("외부 스크립트 temp2");
+}
